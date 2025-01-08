9e81cd400dda23738b643f859e8a06fe9336e87c
Davi Rocha <davirochafortesbezerra@gmail.com>
2025-01-08 00:42:35 -0300

    add linha de como funciona o Processo seletivo.

--- portal-v2/script.js
+++ portal-v2/script.js
@@ -1,11 +1,2 @@
-window.addEventListener("scroll",()=>{
-    const scrollDiv = document.querySelector(".barraNavegacao");
 
-    if(window.scrollY>50){
-        scrollDiv.classList.add("expandido");
-
-
-    }else{
-        scrollDiv.classList.remove("expandido");
-    }
-});
+  
