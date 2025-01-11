c9e1774e88dc8c31ace95f8c4b856f11b13d8e67
Fernando Cavalcante <fernando65620@estudante.ifb.edu.br>
2025-01-11 18:07:32 -0300

    Criei uma pasta para a edição dos scripts para a renderização dos posts.

---
+++ Testes_Scripts v2/Versao_Antiga/script.js
@@ -0,0 +1,43 @@
+// Código que renderiza os posts na pagina inicial
+document.addEventListener("DOMContentLoaded", async () => {
+    const resposta = await fetch('posts.json');
+    const posts = await resposta.json();
+  
+    const postagens = document.getElementById('postagens');
+    postagens.innerHTML = posts.map(post => `
+      <article class="post">
+        <div class="header-postagem">
+            <img src="${post.imagem}" alt="${post.titulo}" class="imagem-postagem"/>
+            <h2><a href="post.html?id=${post.id}">${post.titulo}</a></h2>
+        </div>
+         <p>${post.previa}</p>
+      </article>
+    `).join('');
+  });
+// Busca o post e renderiza ele na pagina posts.html
+document.addEventListener("DOMContentLoaded", async () => {
+    const params = new URLSearchParams(window.location.search);
+    const postId = params.get('id');
+  
+    if (postId) {
+      const resposta = await fetch('posts.json');
+      const posts = await resposta.json();
+      const post = posts.find(p => p.id == postId);
+  
+      if (post) {
+        document.getElementById('postHeader').innerHTML = `
+          <h1>${post.titulo}</h1>
+          <p>Postado em ${post.data}</p>
+        `;
+        document.getElementById('postContent').innerHTML = `
+          <img src="${post.imagem}" alt="${post.titulo}" class="imagem-postagem-grande"/> 
+          <p>${post.conteudo}</p>
+        `;
+      } else {
+        document.getElementById('postContent').innerHTML = '<p>Postagem não encontrada.</p>';
+      }
+    }
+  });
+ 
+
+    
